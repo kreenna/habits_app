41eeb6452904178e7fde56c3c866c54442f8bb8c
Arina <rudenskaaarina@gmail.com>
2025-08-27 22:14:16 +0300

    registration and user viewset

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,19 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'habit',
     'users',
-
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import CustomUser
+from .permissions import IsOwner, IsModerator
+from .serializers import UserSerializer
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = CustomUser.objects.all()
+
+    def get_permissions(self):
+        if self.action in ["create"]:
+            self.permission_classes = [AllowAny]
+        elif self.action in ["update", "partial_update"]:
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        elif self.action in ["list", "retrieve"]:
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        else:
+            self.permission_classes = [IsAuthenticated, IsOwner | IsModerator]
+
+        return [permission() for permission in self.permission_classes]
